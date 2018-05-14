48878a9b7170c89c7611d51e2f45685a6eeccbe5
Olegzandr VD <alex@oresoftware.com>
2018-05-13 18:32:46 -0700

    pdev:'set'

--- shell.sh
+++ shell.sh
@@ -4,6 +4,15 @@
 
 my_dir="$(dirname "$(realpath "$0")")"
 
-for f in "$my_dir/bash/"*; do echo "$f" && . "$f"; done
+
+DIRN="$(dirname "$0")"
+RL="$(readlink "$0")";
+EXECDIR="$(dirname $(dirname "$RL"))";
+MYPATH="$DIRN/$EXECDIR";
+X="$(cd $(dirname ${MYPATH}) && pwd)/$(basename ${MYPATH})"
+
+echo "xxx => $X"
+
+for f in "$X/bash/"*; do echo "$f" && . "$f"; done
 
 
