63793604d9152fc565e77a2a001d478ee6c8f896
Olegzandr VD <alex@oresoftware.com>
2018-05-13 18:36:14 -0700

    pdev:'set'

--- shell.sh
+++ shell.sh
@@ -2,17 +2,7 @@
 
 # https://unix.stackexchange.com/questions/17717/refer-to-a-file-under-the-same-directory-of-a-script-found-in-path
 
-my_dir="$(dirname "$(realpath "$0")")"
 
-
-DIRN="$(dirname "$0")"
-RL="$(readlink "$0")";
-EXECDIR="$(dirname $(dirname "$RL"))";
-MYPATH="$DIRN/$EXECDIR";
-X="$(cd $(dirname ${MYPATH}) && pwd)/$(basename ${MYPATH})"
-
-echo "xxx => $X"
-
-for f in "$X/bash/"*; do echo "$f" && . "$f"; done
+for f in "$HOME/.oresoftware/bash/"*; do echo "$f" && . "$f"; done
 
 
