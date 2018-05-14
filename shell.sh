07e9ad1069dc1786b5abee93ccb0cd61ddee6ac2
Olegzandr VD <alex@oresoftware.com>
2018-05-13 21:44:00 -0700

    pdev:'set'

--- shell.sh
+++ shell.sh
@@ -2,7 +2,16 @@
 
 # https://unix.stackexchange.com/questions/17717/refer-to-a-file-under-the-same-directory-of-a-script-found-in-path
 
-
 for f in "$HOME/.oresoftware/bash/"*; do echo "$f" && . "$f"; done
 
 
+ores_shell_re_source(){
+  for f in "$HOME/.oresoftware/bash/"*; do echo "$f" && . "$f"; done
+}
+
+ores_shell_ls(){
+  ls "$HOME/.oresoftware/bash";
+}
+
+export -f ores_re_source;
+export -f ores_shell_ls;
